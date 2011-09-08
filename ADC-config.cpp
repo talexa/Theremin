53d82a5d841d3472360b895660ecbf82117a5802
Tim Alexander <talexa6@lsu.edu>
2011-09-08 16:38:10 -0500

    Adding a file from Lab Computer 12

---
+++ ADC-config.cpp
@@ -0,0 +1,15 @@
+#include mbed.h
+
+ #define CLKS_PER_SAMPLE 65 //This is the number of cycles it takes to convert a sample
+
+
+////////////////////Configuring the ADC for DMA + Burst Mode Operation////////////////////
+
+//	1. Set PCADC bit in PCONP register to Power On
+LPC_SC->PCONP |= (1 << 12);
+
+//	2. Set PCLK_ADC bit in PCLKSEL0 Register
+LPC_SC->PCLKSEL0 &= ~(0x3 << 24);
+
+//	3. Enable Pins in PINSEL registers
+LPC_SC 
