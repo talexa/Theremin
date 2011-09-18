5eff21f08c367d78306e14bb63d98ac9bfa359ed
Tim Alexander <azul@Azul.(none)>
2011-09-17 21:46:37 -0500

    Updated

--- dma_example.cpp
+++ dma_example.cpp
@@ -9,12 +9,32 @@
 #include mbed.h
 #include MODDMA.h
 
+#define SAMPLE_BUFFER_LENGTH 32
+
+//Will be used for Interrupt Service Routine
+bool dmaTransferComplete = false;
+
 Serial pc(USBTX,USBRX);
 MODDMA dma;
+
+//Function Prototypes
+void TC0_callback(void);
+void ERR0_callback(void);
 
 int main(){
 	//Setting the Baud Rate for the Serial Monitor
 	pc.baud(115200);
+	pc.printf("Testing out ADC with DMA/n");
+	pc.printf("========================/n");
+
+	
+	//Buffer to hold most recent ADC conversion
+	uint32_t adcInputBuffer[SAMPLE_BUFFER_LENGTH]; 
+	
+	//Attaching the Callback Functions to the dma object
+	dma.attach_tc( &dmaTCCallback );
+    dma.attach_err( &dmaERRCallback );
+	
 	
 	//Creating a new MODDMA Configuration Object
 	MODDMA_Config *testconfig = new MODDMA_config;
@@ -30,6 +50,9 @@ int main(){
 	->attach_tc     ( &TC0_callback )
 	->attach_err    ( &ERR0_callback )
 	;
+	
+	dma.Setup(config);
+	dma.Enable(config);
 	
 	
 	
