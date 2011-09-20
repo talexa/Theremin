e03a4b10a66198d0af7092a464e77b737529ba25
Tim Alexander <azul@Azul.(none)>
2011-09-20 10:17:32 -0500

    Updated for Milestone

--- dma_example.cpp
+++ dma_example.cpp
@@ -1,19 +1,23 @@
 /*
 	Timothy Alexander
-
-	These are the configuration settings for the DMA controller using MODDMA library
-	This configuration uses a single channel of the controller for the ADC convertor
+	MileStone #1 Code
+	Objective: Print Output of ADC to Serial Monitor
 
 */
 
 #include mbed.h
 #include MODDMA.h
-
+#include MODSERIAL.h
 #define SAMPLE_BUFFER_LENGTH 32
+#define NUM_OF_SAMPLES 1
+
 
 //Will be used for Interrupt Service Routine
 bool dmaTransferComplete = false;
 
+//Need to make a buffer for taking in A/D Convertor Samples
+uint32_t adBuffer[NUM_OF_SAMPLES * 3];
+b
 Serial pc(USBTX,USBRX);
 MODDMA dma;
 
@@ -23,25 +27,45 @@ void dmaERRCallback(void);
 void TC0_callback(void);
 void ERR0_callback(void);
 
+extern "C" void TIMER1_handler(void) __irq {
+    led2 = !led2; // Show life
+    dma.Setup( conf ); 	// Prepare Transfer
+    dma.Enable( conf );
+    LPC_ADC->ADCR |= (1UL << 16); // ADC burst mode
+    LPC_ADC->ADINTEN = 0x100;     // Do all channels.
+    LPC_TIM1->IR = 1; // Clr timer1 irq.
+}
+
+
 int main(){
+	uint32_t adcInputBuffer[SAMPLE_BUFFER_LENGTH]; 		//Buffer to hold most recent ADC conversion
+	memset(adBuffer, 0, sizeof(adcInputBuffer));		//Clear the Buffer
+
+	NVIC_DisableIRQ(ADC_IRQn);							//ADC will trigger IRQ, so NVIC must be disabled
+	
+	LPC_SC->PCONP 		|= (1UL << 12);					//Power On ADC
+	LPC_SC->PCLKSEL0 	&= ~(3UL << 24);				//PCLK = CCLK/4 96M/4 = ~24Mhz
+
+	//Configuring Pins for ADC
+	LPC_PINCON->PINSEL1 &= ~(3UL << 14);	// Mbed Pin 15
+	LPC_PINCON->PINSEL1 |= (1UL << 14);
+	
+	/*
+	 * Not Going to use this for Milestone 1
+	LPC_PINCON->PINSEL1 &= ~(3UL << 16);	// Mbed Pin 16
+	LPC_PINCON->PINSEL1 |= (3UL << 16);
+	*/
+	
 	//Setting the Baud Rate for the Serial Monitor
 	pc.baud(115200);
-	pc.printf("Testing out ADC with DMA/n");
+	pc.printf("Getting Values!!!/n");
 	pc.printf("========================/n");
-
-	
-	//Buffer to hold most recent ADC conversion
-	uint32_t adcInputBuffer[SAMPLE_BUFFER_LENGTH]; 
-	
-	//Attaching the Callback Functions to the dma object
-	dma.attach_tc( &dmaTCCallback );
-    dma.attach_err( &dmaERRCallback );
 	
 	
 	//Creating a new MODDMA Configuration Object
-	MODDMA_Config *testconfig = new MODDMA_config;
+	MODDMA_Config *config = new MODDMA_config;
 	
-	testconfig
+	config
 	->ChannelNum	( MODDMA::Channel_0)//First Channel Available
 	->srcMemAddr 	( 0 )				//No Memory Address since Source will be ADC
 	->transferType 	( MODDMA::p2m)  	//Peripheral to Memory transfer
@@ -49,22 +73,14 @@ int main(){
 	->srcConn 		( MODDMA::ADC) 		//Source is ADC
 	->dstConn		( 0 )    
 	->dmaLLI		( 0 )
-	->attach_tc     ( &TC0_callback )
-	->attach_err    ( &ERR0_callback )
+	->attach_tc     ( &TC0_callback )	//Attaching Callback Functions...
+	->attach_err    ( &ERR0_callback )	//To the DMA Controller
 	;
 	
 	dma.Setup(config);
 	dma.Enable(config);	
 }
 
-//DMA Controller ERR IRQ Callback
-void dmaERRCAllback(void){
-	error("There be an error");
-}
-//DMA Controller TC IRQ callback
-void dmaERRCallback(void){
-	
-}
 //Configuration callback on TC
 void TC0_callback(void){
 	MODDMA_Config *config = dma.getConfig();
